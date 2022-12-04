cfff787081c9a16ae7e3fd2dcf2cf37d1e426691
brjalan <74614880+arismaramar@users.noreply.github.com>
2022-12-04 14:41:14 +0700

    Update about.sh

--- system/about.sh
+++ system/about.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #wget https://github.com/${GitUser}/
-GitUser="Denisuprapto"
+GitUser="arismaramar"
 #IZIN SCRIPT
 MYIP=$(curl -sS ipv4.icanhazip.com)
 echo -e "\e[32mloading...\e[0m"
@@ -42,7 +42,7 @@ figlet " BY  V - Code" | lolcat
 echo -e "\e[32;1m.-----------------------------------------------.\e[0m"
 echo -e "\e[32;1m|               \e[36;1mINFO SCRIPT VPS\e[0m                 \e[32;1m|\e[0m"
 echo -e "\e[32;1m'-----------------------------------------------'\e[0m"
-echo -e "           \e[031;1m> Premium Script By TESTVPN <\e[0m" | lolcat
+echo -e "           \e[031;1m> Premium Script By TESTVPNanggun-vpn <\e[0m" | lolcat
 echo -e " \e[0;32m_______________________________________________\e[0m"
 echo -e "\e[0;32m|    \e[1;35mFor OS Debian 10 & Ubuntu 20.04 64 bit     \e[0;32m|\e[0m"
 echo -e "\e[0;32m|  \e[1;35mFor VPS with KVM and VMWare virtualization   \e[0;32m|\e[0m"
